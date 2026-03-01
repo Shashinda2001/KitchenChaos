64abcd2fc03074b077fc9ac6410bfab586bc9b3d
Shashinda Adithya <142221873+Shashinda2001@users.noreply.github.com>
2026-03-01 16:27:05 +0530

    Add basic WASD player movement with smooth rotation  Implement player movement and direction-based rotation  Create Player controller with normalized movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] private float moveSpeed=7f;
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector2 inputVector = new Vector2(0,0);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = +1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = +1;
+        }
+
+        // Normalize the input vector to ensure consistent movement speed in all directions
+        inputVector = inputVector.normalized;
+
+        // Convert the 2D input vector to a 3D movement direction (assuming movement on the XZ plane)
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+        transform.position += moveDir * Time.deltaTime * moveSpeed;
+
+        // Smoothly rotate the player to face the movement direction
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+    }
+}
