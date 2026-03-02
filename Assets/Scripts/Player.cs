2a7cd2e39e560ac9532eb77bdc08bb7bfac44686
Shashinda Adithya <142221873+Shashinda2001@users.noreply.github.com>
2026-03-02 18:13:48 +0530

    Check for potential collisions in the movement direction using a capsule cast,If there's an obstacle in the movement direction, try to move only along the X or Z axis

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,6 +18,38 @@ public class Player : MonoBehaviour
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         // Convert the 2D input vector to a 3D movement direction (assuming movement on the XZ plane)
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+
+        // Check for potential collisions in the movement direction using a capsule cast
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = .7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+
+        if (!canMove)
+        {
+            //can't move towards moveDir, try only X movement
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = moveDir.x != 0 && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+
+            if (canMove)
+            {
+                moveDir = moveDirX;
+            }
+            else
+            {
+                //can't move towards moveDir, try only Z movement
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = moveDir.z != 0 && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+                if (canMove)
+                {
+                    moveDir = moveDirZ;
+                }
+            }
+
+        }
+       
+
+        if (canMove)
         transform.position += moveDir * Time.deltaTime * moveSpeed;
 
         isWalking =moveDir != Vector3.zero;
