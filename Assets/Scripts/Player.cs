435b1077f7425a93c14b69bfd251c8f0057506a0
Shashinda Adithya <142221873+Shashinda2001@users.noreply.github.com>
2026-03-02 17:18:57 +0530

    create a separate script for game input

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,34 +7,15 @@ public class Player : MonoBehaviour
 {
 
     [SerializeField] private float moveSpeed=7f;
+    [SerializeField] private GameInput gameInput;
 
     private bool isWalking;
 
     // Update is called once per frame
     private void Update()
     {
-        Vector2 inputVector = new Vector2(0,0);
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
-
-        // Normalize the input vector to ensure consistent movement speed in all directions
-        inputVector = inputVector.normalized;
-
+       
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         // Convert the 2D input vector to a 3D movement direction (assuming movement on the XZ plane)
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
         transform.position += moveDir * Time.deltaTime * moveSpeed;
