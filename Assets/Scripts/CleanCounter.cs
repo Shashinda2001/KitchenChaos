db9b3aabf54fc032cea8548577d8c42c6e49d2ed
Shashinda Adithya <142221873+Shashinda2001@users.noreply.github.com>
2026-03-25 22:02:56 +0530

    Implement object spawning on CleanCounter using a designated attachment point.

--- Assets/Scripts/CleanCounter.cs
+++ Assets/Scripts/CleanCounter.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class CleanCounter : MonoBehaviour
 {
-    
+    [SerializeField] private Transform counterTopPoint;
+    [SerializeField] private Transform TomatoPrefab;
     public void Interact() {         
         Debug.Log("Interacting with clean counter");
+        Transform tomatoTransform = Instantiate(TomatoPrefab, counterTopPoint);
+         tomatoTransform.localPosition = Vector3.zero;
     }
 }
